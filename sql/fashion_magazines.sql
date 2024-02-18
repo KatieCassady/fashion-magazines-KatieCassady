f1b099b6863b6828f27c8cb0153f2c4c89c0ac17
Katie Cassady <katie@cassadyweb.com>
2024-02-18 09:12:41 -0500

    partial

--- sql/fashion_magazines.sql
+++ sql/fashion_magazines.sql
@@ -1,1 +1,10 @@
--- Add your sql here
+SELECT *
+FROM orders
+JOIN subscriptions
+    ON orders.customer_id = subscriptions.customer_id;
+
+SELECT *
+FROM orders
+JOIN subscriptions
+  ON orders.subscription_id = subscriptions.subscription_id
+WHERE subscriptions.length >= 1
