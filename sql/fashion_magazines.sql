4c2a7c6c34d452ff2da47cfb0fd184151cb87ab8
Katie Cassady <katie@cassadyweb.com>
2024-02-18 10:12:35 -0500

    keep getting 'can only execute one command at a time'

--- sql/fashion_magazines.sql
+++ sql/fashion_magazines.sql
@@ -7,12 +7,17 @@ FROM orders
 JOIN customers
 	ON orders.order_id = customers.customer_id;
 
-SELECT *
-FROM subscriptions
-WHERE subscriptions.description = "unpaid",
-     subscriptions.subscription_length * subscriptions.price_per_month AS amount_due
-FROM orders
-GROUP BY customer_name AND amount_due;
+WITH previous_query AS (SELECT *
+	FROM subscriptions
+	JOIN orders
+		ON orders.customer_id = subscriptions.subscription_id
+	WHERE orders.order_status = "unpaid"
+		AND subscriptions.description = 'Fashion Magazine';
 )
+
+SELECT customers.customer_name, 
+   price_per_month * subscription_length AS amount_due
+FROM previous_query
+
  
 PRINTF()
